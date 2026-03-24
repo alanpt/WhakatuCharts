76d7f5413c45452b18d961aa511ee0173a4a23a6
alanpt <ALANPT@GMAIL.COM>
2026-03-24 16:38:49 +1300

    Refactor service worker fetch event handling

--- sw.js
+++ sw.js
@@ -1,42 +1,33 @@
-self.addEventListener('fetch', function(event) {
-    if (event.request.destination === 'document') {
-        event.respondWith(fetch(event.request).catch(function() {
-            return caches.match(event.request);
-        }));
-    } else if (event.request.destination === 'audio' || event.request.destination === 'image') {
-        event.respondWith(fetch(event.request).then(function(response) {
-            let responseToCache = response.clone();
-            caches.open('track-cache').then(function(cache) {
-                cache.put(event.request, responseToCache);
-            });
-            return response;
-        }).catch(function() {
-            return caches.match(event.request);
-        }));
+// Service Worker - sw.js
+
+const CACHE_NAME = 'whakatu-charts-cache-v1';
+const NETWORK_FIRST_URL = 'tracks.json';
+const CACHE_FIRST_URLS = ['/audio/', '/images/'];
+
+self.addEventListener('fetch', event => {
+    const requestUrl = new URL(event.request.url);
+
+    if (requestUrl.pathname === NETWORK_FIRST_URL) {
+        event.respondWith(networkFirst(event));
+    } else if (CACHE_FIRST_URLS.some(url => requestUrl.pathname.indexOf(url) === 0)) {
+        event.respondWith(cacheFirst(event));
     }
 });
 
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open('track-cache').then(function(cache) {
-            return cache.addAll([
-                'tracks.json',
-                'index.html'
-            ]);
-        })
-    );
-});
+async function networkFirst(event) {
+    try {
+        const response = await fetch(event.request);
+        return response;
+    } catch (error) {
+        return caches.match(event.request);
+    }
+}
 
-self.addEventListener('activate', function(event) {
-    event.waitUntil(
-        caches.keys().then(function(cacheNames) {
-            return Promise.all(
-                cacheNames.map(function(cacheName) {
-                    if (cacheName !== 'track-cache') {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
-});
+async function cacheFirst(event) {
+    const cache = await caches.open(CACHE_NAME);
+    const cachedResponse = await cache.match(event.request);
+    if (cachedResponse) {
+        return cachedResponse;
+    }
+    return fetch(event.request);
+}
