6517722361e39008ca7d32201ba8fa3ee6f44787
alanpt <ALANPT@GMAIL.COM>
2026-03-24 17:28:36 +1300

    Revert to Feb 24 version

--- sw.js
+++ sw.js
@@ -1,36 +1,34 @@
-var CACHE = 'whakatu-v1';
-var STATIC = ['/', '/index.html', '/manifest.json', '/logo.png'];
+const CACHE = 'whakatu-v1';
+const STATIC = ['/', '/index.html', '/manifest.json', '/logo.png'];
 
-self.addEventListener('install', function(e) {
-  e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(STATIC); }));
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(STATIC)));
   self.skipWaiting();
 });
 
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(function(keys) {
-      return Promise.all(keys.filter(function(k) { return k !== CACHE; }).map(function(k) { return caches.delete(k); }));
-    })
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
   );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', e => {
   // Network-first for tracks.json so new tracks appear after push
-  if (e.request.url.indexOf('tracks.json') !== -1) {
+  if (e.request.url.includes('tracks.json')) {
     e.respondWith(
-      fetch(e.request).catch(function() { return caches.match(e.request); })
+      fetch(e.request).catch(() => caches.match(e.request))
     );
     return;
   }
   // Cache-first for everything else
   e.respondWith(
-    caches.match(e.request).then(function(hit) {
-      return hit || fetch(e.request).then(function(res) {
-        var clone = res.clone();
-        caches.open(CACHE).then(function(c) { c.put(e.request, clone); });
-        return res;
-      });
-    })
+    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }))
   );
 });
